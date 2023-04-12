51588b009ffc2d7c96a230fb2963bceb3d5f254c
Anton Vorontsov <anton.vorontsov@infopulse.com>
2023-04-12 18:25:04 +0300

    setup handler

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,24 @@ import * as url from 'url';
 const  GetMultipleIntegerHandler = (req: IncomingMessage, res: ServerResponse) => {
   const parsedUrl = url.parse(req.url ?? '', true);
   const query = parsedUrl.query;
-  console.log(query);
-  if(query.number) {
+  const number = Number(query?.number);
+  if (!isNaN(number)) {
+    let result = '';
 
+    if (number % 3 === 0) {
+      result += 'G';
+    }
+
+    if (number % 5 === 0) {
+      result += 'N';
+    }
+
+    if (!result) {
+      result = number.toString();
+    }
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ result }));
   } else {
     res.writeHead(400, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ error: 'Invalid input' }));
@@ -17,14 +32,16 @@ const  GetMultipleIntegerHandler = (req: IncomingMessage, res: ServerResponse) =
 };
 
 const router = {
-  '/': {
+  '/api/': {
     GET: GetMultipleIntegerHandler,
   }
 };
 
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+  const parsedUrl = url.parse(req.url ?? '', true);
   const method = req.method ?? 'GET';
-  const handler = (router as any)[req.url ?? ''][method];
+
+  const handler = (router as any)[parsedUrl.pathname ?? '']?.[method];
 
   if(handler) {
     handler(req, res);
